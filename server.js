4d3762ddb5a1d9531bf2e93a32d8f729a2e06d2d
RayOconnor <roconnorc@gmail.com>
2017-01-17 22:11:57 -0500

    link to github

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
+app.get('/times', function(request, response) {
+    var result = ''
+    var times = process.env.TIMES || 5
+    for (i=0; i < times; i++)
+      result += i + ' ';
+  response.send(result);
+});
+
 require ("./test/app.js")(app);
 
 var port = process.env.PORT || 3000;
