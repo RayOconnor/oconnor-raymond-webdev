74fe602f75d0f540f615e5356455088520bc85f6
RayOconnor <roconnorc@gmail.com>
2017-03-18 14:44:44 -0400

    add flickr functionality

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -66,8 +66,8 @@
         controllerAs: "model"
       })
       .when("/user/:uid/website/:wid/page/:pid/widget/:wgid/search",{
-        templateUrl: 'views/widget/templates/widget-edit.view.client.html'
-        ,controller: "WidgetEditController",
+        templateUrl: 'views/widget/templates/widget-flickr-search.view.client.html'
+        ,controller: "WidgetFlickrSearchController",
         controllerAs: "model"
       })
       .otherwise({
